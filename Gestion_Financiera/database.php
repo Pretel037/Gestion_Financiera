f038cb3805eca7fec86fdb817dad1ea3a2950510
Pretel037 <george69pretel@gmail.com>
2024-10-28 13:57:12 -0500

    pruebas finales

--- Gestion_Financiera/database.php
+++ Gestion_Financiera/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,14 +42,14 @@ return [
             'synchronous' => null,
         ],
 
-       'mysql' => [
+        'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'instituto'),
-            'username' => env('DB_USERNAME', 'ixocakuy_instituto'),
-            'password' => env('DB_PASSWORD', 'Instituto123'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -61,7 +61,6 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
 
         'mariadb' => [
             'driver' => 'mariadb',
